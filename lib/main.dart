000f134cc2d51a5882216f6efeb8603afbe810d1
Taravann Heng <heng.t.taravann@gmail.com>
2024-03-03 14:27:49 +0700

    feat(appBar): add app bar

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,42 @@ class Youtube extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color almostBlack = Color.fromRGBO(40, 40, 40, 1);
+    Color white = Color.fromRGBO(255, 255, 255, 1);
+    Color youtubeRed = Color.fromRGBO(255, 0, 0, 1);
+
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
-          appBar: AppBar(),
+          appBar: AppBar(
+            backgroundColor: almostBlack,
+            leading: Image.asset(
+              'images/yt_logo_rgb_dark.png',
+            ),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.cast_outlined,
+                  color: white,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.notifications_outlined,
+                  color: white,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.search,
+                  color: white,
+                ),
+              ),
+            ],
+          ),
           // body: ,
           // bottomNavigationBar: ,
         ),
