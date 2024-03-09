5ee883f665ac8974c0b83527cbb156c7368bd926
Taravann Heng <heng.t.taravann@gmail.com>
2024-03-09 12:47:13 +0700

    feat(subscription-screen): add subscription screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,99 @@ import 'package:flutter/material.dart';
 
 // screen imports
 import 'screens/home_screen.dart';
+import 'screens/subscription_screen.dart';
+
+// utils imports
+import 'utils/colors/ytcolors.dart';
 
 void main() => runApp(const Youtube());
 
-class Youtube extends StatelessWidget {
+class Youtube extends StatefulWidget {
   const Youtube({super.key});
+
+  @override
+  State<Youtube> createState() => _YoutubeState();
+}
+
+class _YoutubeState extends State<Youtube> {
+  int _selectedIndex = 0;
+  YTColors ytColors = YTColors();
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomeScreen(),
+    SubscriptionScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: ytColors.almostBlack,
+          leading: Image.asset(
+            'images/yt_logo_rgb_dark.png',
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.cast_outlined,
+                color: ytColors.white,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.notifications_outlined,
+                color: ytColors.white,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.search,
+                color: ytColors.white,
+              ),
+            ),
+          ],
+        ),
+        body: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: ytColors.white,
+          unselectedItemColor: ytColors.white,
+          backgroundColor: ytColors.almostBlack,
+          items: [
+            BottomNavigationBarItem(
+              icon: _selectedIndex == 0
+                  ? Icon(Icons.home_filled)
+                  : Icon(Icons.home_outlined),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: _selectedIndex == 1
+                  ? Icon(Icons.subscriptions)
+                  : Icon(Icons.subscriptions_outlined),
+              label: 'Subscriptions',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle_outlined),
+              label: 'You',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+        ),
+      ),
       routes: {
-        '/': (context) => const HomeScreen(),
+        '/subscription': (context) => const SubscriptionScreen(),
       },
     );
   }
--- lib/screens/subscription_screen.dart
+++ lib/screens/subscription_screen.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 // component imports
 import '../widgets/chip_list.dart';
 import '../widgets/card.dart';
-import '../widgets/shorts.dart';
+import '../widgets/channel_list.dart';
 
 // utils imports
 import '../utils/colors/ytcolors.dart';
 import '../utils/mock_data/mock_data.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class SubscriptionScreen extends StatelessWidget {
+  const SubscriptionScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +21,7 @@ class HomeScreen extends StatelessWidget {
       body: ListView(children: [
         Column(
           children: [
+            YTChannelList(),
             ChipList(),
             SizedBox(
               height: 8,
@@ -31,7 +32,6 @@ class HomeScreen extends StatelessWidget {
               imgSrc: cardMockData[0]['imgSrc'],
               metaData: cardMockData[0]['metaData'],
             ),
-            Shorts(),
             for (int i = 1; i <= 3; i++)
               CustomCard(
                 avatarSrc: cardMockData[i]['avatarSrc'],
